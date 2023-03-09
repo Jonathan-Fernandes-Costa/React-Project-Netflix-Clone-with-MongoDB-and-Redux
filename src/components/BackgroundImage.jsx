c2661aa8eaff4eb9bff2e0b224b7bc2083111c16
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-03-09 17:29:06 -0300

    Estlizando a tela de signup

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,21 +1,20 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 import background from "../assets/login.jpg";
-function BackgroundImage() {
-    return (
-        <Container>
-            <img src={background} alt='background' />
-        </Container>
-    )
+
+export default function BackgroundImage() {
+  return (
+    <Container>
+      <img src={background} alt="background" />
+    </Container>
+  );
 }
 
-
 const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  img {
     height: 100vh;
     width: 100vw;
-    img{
-        height: 100vh;
-        width: 100vw;
-    }
+  }
 `;
-export default BackgroundImage
